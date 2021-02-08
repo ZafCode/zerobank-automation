45534d3e3f4e1d3296e417945aa2715e3eac0dcd
Serkan <serkara2de@hotmail.com>
2021-02-08 01:30:01 +0100

    updates

--- src/test/java/com/zerobank/pages/BasePage.java
+++ src/test/java/com/zerobank/pages/BasePage.java
@@ -1,16 +1,10 @@
 package com.zerobank.pages;
 
-import com.zerobank.utilities.BrowserUtils;
 import com.zerobank.utilities.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Collections;
 import java.util.List;
 
 public abstract class BasePage {
@@ -33,10 +27,4 @@ public abstract class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
     }
-
-
-
-
-
-
 }
